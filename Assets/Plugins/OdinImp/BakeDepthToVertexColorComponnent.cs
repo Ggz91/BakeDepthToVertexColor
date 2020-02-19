55bc0dcaf7d8a1083af608cc363167b15d957450
529038378 <529038378@qq.com>
2020-02-19 16:18:11 +0800

    odin editor window

---
+++ Assets/Plugins/OdinImp/BakeDepthToVertexColorComponnent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+
+public class BakeDepthToVertexColorComponent
+{
+    [InfoBox("把深度信息映射到网格的定点色上")]
+    [MinValue(1)]
+    public Vector2Int Size = new Vector2Int(100, 100);
+
+    [MinValue(0.1f)]
+    public Vector2 UnitSize = new Vector2(0.1f, 0.1f);
+}
---
+++ Assets/Plugins/OdinImp/EditorWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+
+public class OdinEditorWindow : OdinMenuEditorWindow
+{
+    [MenuItem("CusPlugin/OdinEditroWindow")]
+    private static void ShowWindow()
+    {
+        GetWindow<OdinEditorWindow>().Show();
+    }
+    static BakeDepthToVertexColorComponent m_bake_depth_to_vertex_color_component = new BakeDepthToVertexColorComponent();
+    static OdinMenuTree tree = new OdinMenuTree();
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        tree.Selection.SupportsMultiSelect = false;
+        tree.Add("EditorWindow", m_bake_depth_to_vertex_color_component);
+        return tree;
+    }
+}
